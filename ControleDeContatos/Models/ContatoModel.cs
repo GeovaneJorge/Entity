dd8ee81a59acf3dd0914d20c154edac17f8484c4
Geovane Jorge <geovane.jorge@gmail.com>
2022-08-16 13:29:53 -0300

    Enviando o banco de Dados

---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Models
+{
+	public class ContatoModel
+	{
+		//Criando as tabelas do Banco de Dados com Entity Framework
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Email { get; set; }
+		public string Celular { get; set; }
+	}
+}
---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,37 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleDeContatos.Repositorio
+{
+	public class ContatoRepositorio : IContatoRepositorio
+	{
+		private readonly BancoContext _bancoContext;
+
+		public ContatoRepositorio(BancoContext bancoContext)
+		{
+			_bancoContext = bancoContext;
+		}
+
+		public ContatoModel ListarPorId(int id)
+		{
+			return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+		}
+
+		public List<ContatoModel> BuscarTodos()
+		{
+			return _bancoContext.Contatos.ToList();
+		}
+		public ContatoModel Adicionar(ContatoModel contato)
+		{
+			_bancoContext.Contatos.Add(contato);
+			_bancoContext.SaveChanges();
+			return contato;
+		}
+
+		
+	}
+}
